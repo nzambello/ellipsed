8c3102e86710ef012f43aa87f29e78c64eb46cea
nzambello <nicola.zambello@student.unife.it>
2017-08-19 00:14:31 +0200

    aggiunta label licenza RMS nel codice

--- ellipsed.js
+++ ellipsed.js
@@ -1,16 +1,35 @@
-const ellipsed = (selector, rows) => {
-  let elements = document.querySelectorAll(selector);
+function ellipsed(selector, rows) {
+  /*   Copyright (C) 2017  Nicola Zambello
+   *
+   *		https://github.com/nzambello/ellipsed
+   *
+   *    The JavaScript code in this page is free software: you can
+   *    redistribute it and/or modify it under the terms of the GNU
+   *    General Public License (GNU GPL) as published by the Free Software
+   *    Foundation, either version 3 of the License, or (at your option)
+   *    any later version.  The code is distributed WITHOUT ANY WARRANTY;
+   *    without even the implied warranty of MERCHANTABILITY or FITNESS
+   *    FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+   *
+   *    As additional permission under GNU GPL version 3 section 7, you
+   *    may distribute non-source (e.g., minimized or compacted) forms of
+   *    that code without the copy of the GNU GPL normally required by
+   *    section 4, provided you include this license notice and a URL
+   *    through which recipients can access the Corresponding Source.
+   */
 
-  for (let el of elements) {
+  var elements = document.querySelectorAll(selector);
+
+  for (var el of elements) {
     if (rows) {
-      let splittedText = el.innerText.split(' ');
-      let rowsWrapped = 0;
-      let textBeforeWrap = '';
+      var splittedText = el.innerText.split(' ');
+      var rowsWrapped = 0;
+      var textBeforeWrap = '';
 
       el.innerText = '';
-      let elHeight = window.getComputedStyle(el).height;
+      var elHeight = window.getComputedStyle(el).height;
 
-      for (let token of splittedText) {
+      for (var token of splittedText) {
         if (el.innerText.length) {
           el.innerText = el.innerText.concat(' ').concat(token).concat('...');
         }
