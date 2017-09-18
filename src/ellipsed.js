8516b7ed1e7521875ea0779cb48cd41efd99fa1b
Piero Nicolli <piero.nicolli@yahoo.it>
2017-09-18 19:32:28 +0200

    wip: functions instead of for

--- src/ellipsed.js
+++ src/ellipsed.js
@@ -1,35 +1,75 @@
-function ellipsed(selector = '', rows = 1) {
-  /*   Copyright (C) 2017  Nicola Zambello
-   *
-   *    https://github.com/nzambello/ellipsed
-   *
-   *    The JavaScript code in this page is free software: you can
-   *    redistribute it and/or modify it under the terms of the GNU
-   *    General Public License (GNU GPL) as published by the Free Software
-   *    Foundation, either version 3 of the License, or (at your option)
-   *    any later version.  The code is distributed WITHOUT ANY WARRANTY;
-   *    without even the implied warranty of MERCHANTABILITY or FITNESS
-   *    FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
-   *
-   *    As additional permission under GNU GPL version 3 section 7, you
-   *    may distribute non-source (e.g., minimized or compacted) forms of
-   *    that code without the copy of the GNU GPL normally required by
-   *    section 4, provided you include this license notice and a URL
-   *    through which recipients can access the Corresponding Source.
-   */
+/*   Copyright (C) 2017  Nicola Zambello
+  *
+  *    https://github.com/nzambello/ellipsed
+  *
+  *    The JavaScript code in this page is free software: you can
+  *    redistribute it and/or modify it under the terms of the GNU
+  *    General Public License (GNU GPL) as published by the Free Software
+  *    Foundation, either version 3 of the License, or (at your option)
+  *    any later version.  The code is distributed WITHOUT ANY WARRANTY;
+  *    without even the implied warranty of MERCHANTABILITY or FITNESS
+  *    FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+  *
+  *    As additional permission under GNU GPL version 3 section 7, you
+  *    may distribute non-source (e.g., minimized or compacted) forms of
+  *    that code without the copy of the GNU GPL normally required by
+  *    section 4, provided you include this license notice and a URL
+  *    through which recipients can access the Corresponding Source.
+  */
 
+function tokensReducer(acc, token) {
+  const { el, elStyle, rowsLimit, rowsWrapped } = acc;
+  if (rowsWrapped === rowsLimit + 1) {
+    return { ...acc };
+  }
+  const textBeforeWrap = el.textContent;
+  const elHeight = elStyle.height;
+  let newRowsWrapped = rowsWrapped;
+  el.textContent = el.textContent.length
+    ? `${el.textContent} ${token}...`
+    : `${token}...`;
+
+  if (parseFloat(elStyle.height) > parseFloat(elHeight)) {
+    newRowsWrapped++;
+
+    if (newRowsWrapped === rowsLimit + 1) {
+      el.textContent = textBeforeWrap[textBeforeWrap.length - 1] === '.'
+        ? `${textBeforeWrap}..`
+        : `${textBeforeWrap}...`;
+      
+      return { ...acc, rowsWrapped: newRowsWrapped };
+    }
+  }
+
+  el.textContent = textBeforeWrap.length
+    ? `${textBeforeWrap} ${token}`
+    : `${token}`;
+
+  return { ...acc, rowsWrapped: newRowsWrapped };
+}
+
+function ellipsed(selector = '', rows = 1) {
   const elements = document.querySelectorAll(selector);
 
   for (const el of elements) {
     const splittedText = el.textContent.split(' ');
-    let rowsWrapped = 0;
-    let textBeforeWrap = '';
+    // let rowsWrapped = 0;
+    // let textBeforeWrap = '';
 
     el.textContent = '';
     const elStyle = window.getComputedStyle(el);
-    let elHeight = elStyle.height;
+    // let elHeight = elStyle.height;
 
-    for (const token of splittedText) {
+    splittedText.reduce(
+      tokensReducer,
+      {
+        el,
+        elStyle,
+        rowsLimit: rows,
+        rowsWrapped: 0,
+      }
+    );
+    /* for (const token of splittedText) {
       if (el.textContent.length) {
         el.textContent = `${el.textContent} ${token}...`;
       } else {
@@ -53,7 +93,7 @@ function ellipsed(selector = '', rows = 1) {
         ? `${textBeforeWrap} ${token}`
         : `${textBeforeWrap}${token}`;
       el.textContent = textBeforeWrap;
-    }
+    } */
   }
 }
 
