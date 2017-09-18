b2646503f3f45ebd7eae4efd27c8852c94cfef83
Piero Nicolli <piero.nicolli@yahoo.it>
2017-09-18 19:50:23 +0200

    Changed default export to named export. Some other minor cleanup.

--- example/index.js
+++ example/index.js
@@ -1,36 +1,36 @@
-const ellipsed = window.ellipsed.default;
+var ellipsis = window.ellipsed.ellipsis;
 
-const reset = () => {
-  let aaa = document.querySelectorAll('.text p')[0];
+function reset() {
+  var aaa = document.querySelectorAll('.text p')[0];
 
-  let loremIpsum = document.querySelectorAll('.text p')[1];
+  var loremIpsum = document.querySelectorAll('.text p')[1];
 
   aaa.innerText = 'A a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a';
 
   loremIpsum.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor lectus nibh. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
-};
+}
 
-const uno = () => {
+function uno() {
   reset();
-  ellipsed('.text p', 1);
-};
+  ellipsis('.text p', 1);
+}
 
-const due = () => {
+function due() {
   reset();
-  ellipsed('.text p', 2);
-};
+  ellipsis('.text p', 2);
+}
 
-const tre = () => {
+function tre() {
   reset();
-  ellipsed('.text p', 3);
-};
+  ellipsis('.text p', 3);
+}
 
-const quattro = () => {
+function quattro() {
   reset();
-  ellipsed('.text p', 4);
-};
+  ellipsis('.text p', 4);
+}
 
-const cinque = () => {
+function cinque() {
   reset();
-  ellipsed('.text p', 5);
-};
+  ellipsis('.text p', 5);
+}
