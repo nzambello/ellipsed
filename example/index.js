f7e956fc9c50d24583ad82083f3574ae12b1688e
nzambello <nicola.zambello@redturtle.it>
2017-10-03 00:06:16 +0200

    added demo input for replaceStr

--- example/index.js
+++ example/index.js
@@ -10,27 +10,31 @@ function reset() {
   loremIpsum.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Pellentesque in ipsum id orci porta dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor lectus nibh. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
 }
 
+function getReplaceStr() {
+  return document.querySelector('.replace-str-input').value;
+}
+
 function oneRow() {
   reset();
-  ellipsis('.text p', 1);
+  ellipsis('.text p', 1, { replaceStr: getReplaceStr() });
 }
 
 function twoRows() {
   reset();
-  ellipsis('.text p', 2);
+  ellipsis('.text p', 2, { replaceStr: getReplaceStr() });
 }
 
 function threeRows() {
   reset();
-  ellipsis('.text p', 3);
+  ellipsis('.text p', 3, { replaceStr: getReplaceStr() });
 }
 
 function fourRows() {
   reset();
-  ellipsis('.text p', 4);
+  ellipsis('.text p', 4, { replaceStr: getReplaceStr() });
 }
 
 function fiveRows() {
   reset();
-  ellipsis('.text p', 5);
+  ellipsis('.text p', 5, { replaceStr: getReplaceStr() });
 }
